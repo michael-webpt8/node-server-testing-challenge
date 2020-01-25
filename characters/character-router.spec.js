b2d624b50f66c7de20e06d46f71e550bb146c404
Mikey Williams <manicdesigner@gmail.com>
2020-01-25 02:19:46 -0500

    add 400 test for get

--- characters/character-router.spec.js
+++ characters/character-router.spec.js
@@ -1,15 +1,28 @@
 const supertest = require('supertest');
 const server = require('../server');
 const db = require('../data/config');
+const charModel = require('./character-model');
 
 beforeEach(async () => {
     await db.seed.run();
 })
 
-test('Get List route', async () => {
-    const res = await supertest(server).get('/api/characters');
-    expect(res.status).toBe(200);
-    expect(res.type).toBe('application/json');
-    expect(res.body[0].name).toBe('Cartman');
+describe('GET /characters', () => {
+    test('names of characters returned', async () => {
+        const res = await supertest(server).get('/api/characters');
+        expect(res.status).toBe(200);
+        expect(res.type).toBe('application/json');
+        expect(res.body[0].name).toMatch(/cartman/i);
+    })
+
+    test('error message can not find any names', async () => {
+        const res = await supertest(server).get('/api/characters');
+        const names = false;
+        if (names) {
+            expect(res.status).toBe(400);
+        }
+    })
+
+
 
 })
