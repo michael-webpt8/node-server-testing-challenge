5336e6346b18387dbf9948117bb68a7b434f7fa3
Michael Williams <manicdesigner@gmail.com>
2020-01-25 10:58:49 -0500

    route post added

--- characters/character-router.js
+++ characters/character-router.js
@@ -3,16 +3,31 @@ const charModel = require('./character-model');
 const router = express.Router();
 
 router.get('/characters', async (req, res, next) => {
-    try {
-        const names = await charModel.list();
-        if (names) {
-            res.status(200).json(names)
-        } else {
-            res.status(400).json({ message: 'error finding names' })
-        }
-    } catch (err) {
-        next(err);
+  try {
+    const names = await charModel.list();
+    if (names) {
+      res.status(200).json(names);
+    } else {
+      res.status(400).json({ message: 'error finding names' });
     }
-})
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/api/characters', async (req, res, next) => {
+  if (!req.body.name) {
+    res.status(400).json({ message: 'name required' });
+  }
+  const newName = {
+    name: req.body.name,
+  };
+  try {
+    const name = await char.charModel.insert(newName);
+    res.status(201).json(name);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
