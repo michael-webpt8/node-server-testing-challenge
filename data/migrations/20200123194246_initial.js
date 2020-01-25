76fd3a21cc05edcfb303dc3d57d56df49e6c9345
Michael Williams <manicdesigner@gmail.com>
2020-01-25 11:02:33 -0500

    unique out of table

--- data/migrations/20200123194246_initial.js
+++ data/migrations/20200123194246_initial.js
@@ -1,11 +1,10 @@
-
-exports.up = async function (knex) {
-    await knex.schema.createTable('characters', tbl => {
-        tbl.increments('id');
-        tbl.string('name', 100).unique().notNullable()
-    })
+exports.up = async function(knex) {
+  await knex.schema.createTable('characters', tbl => {
+    tbl.increments('id');
+    tbl.string('name', 100).notNullable();
+  });
 };
 
-exports.down = async function (knex) {
-    await knex.schema.dropTablesIfExist('characters');
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('characters');
 };
